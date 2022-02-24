28a53fa9e655a75d6feafddc549d2e3c7b4e1996
yjchu1117 <yjchu1117@gmail.com>
2022-02-23 21:18:49 -0600

    Update team.js

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,1 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
 
+class Team extends React.Component {
+    
+    render(){
+        return <Layout>
+        <div>
+            <hi>Hello! We are Team XXX!</hi>
+            <p>Team Member 1</p>
+            <p>Team Member 2</p>
+            <p>Team Member 3</p>
+        </div>
+        </Layout>
+    }
+}
+
+export default Team
