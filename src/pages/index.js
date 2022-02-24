b5f5e9d61efae6acbb12d707a015f500fb79fa46
yjchu1117 <yjchu1117@gmail.com>
2022-02-23 21:30:47 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,6 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <Seo title="All posts" />
       <Bio />
-     
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
