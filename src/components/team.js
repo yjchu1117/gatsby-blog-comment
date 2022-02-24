859f94a0694f13237d4144dc1e827a6ad10fea52
yjchu1117 <yjchu1117@gmail.com>
2022-02-23 21:08:09 -0600

    Create team.js

---
+++ src/components/team.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "/layout"
+
+class Team extends React.Component {
+    
+    render(){
+        return <Layout>
+        <div>
+            <hi>Hello! We are Team XXX!</hi>
+            <p>Team Member 1</p>
+            <p>Team Member 2</p>
+            <p>Team Member 3</p>
+        </div>
+        </Layout>
+    }
+}
+
+export default Team
